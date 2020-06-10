85575925fd030ac23b5184a6782bc9342a2fb319
KimJihoon18 <momo990305@gmail.com>
2020-06-11 01:05:15 +0900

    open tetris frame

--- Client/src/hoon2woon2/Main.java
+++ Client/src/hoon2woon2/Main.java
@@ -1,11 +1,17 @@
 package hoon2woon2;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 import org.psnbtech.Tetris;
 
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+
+import java.awt.event.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 
 public class Main extends JFrame{
 	/**
@@ -24,21 +30,21 @@ public class Main extends JFrame{
 		mainframe.setSize(500, 500); // 크기
 		mainframe.setVisible(true);// 생성
 		mainframe.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		mainframe.setLocationRelativeTo(null);
 
 
-		// JButton singleplaybutton = new JButton("singleplay");
-		// mainframe.add(singleplaybutton);
-		// ActionListener listener = new ActionListener() {
+		JButton singleplaybutton = new JButton("singleplay");
+		singleplaybutton.addActionListener(new ActionListener() {
 
-		//    @Override
-		//    public void actionPerformed(ActionListener e) {
-		client = new Client();
-		tetris = new Tetris(client);
-		tetris.startGame();
-		multi = new MultiPlay(client);
-		//    }
-		// }
-		// singleplaybutton.addActionListener(listener);
+		   @Override
+			public void actionPerformed(ActionEvent e) {
+				client = new Client();
+				tetris = new Tetris(client);
+				tetris.startGame();
+				multi = new MultiPlay(client);
+		   }
+		});
+		mainframe.getContentPane().add(singleplaybutton);
 
 	}
 }
